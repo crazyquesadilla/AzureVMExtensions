3a63dd998a0be559857e80b3b1758e3f2da1ef06
Keifer Hofacker <KHofacker@VisionMS.Net>
2020-08-27 15:00:32 -0700

    disk init script

---
+++ InitDisks.ps1
@@ -0,0 +1,16 @@
+# Source: https://docs.microsoft.com/en-us/azure/virtual-machines/windows/attach-disk-ps#initialize-the-disk
+
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+
+$letters = "E","F"
+$count = 0
+$labels = "Data","Log"
+
+foreach ($disk in $disks) {
+    $driveLetter = $letters[$count]
+    $disk | 
+    Initialize-Disk -PartitionStyle MBR -PassThru |
+    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel $labels[$count] -Confirm:$false -Force
+    $count++
+}
